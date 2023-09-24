318da4c61f5de26dbcbefbf37e8b901a31e509e6
srinivasveerla <vdssrinivas98@gmail.com>
2023-09-24 15:25:31 -0400

    New feature to add my words

--- main.js
+++ main.js
@@ -2,68 +2,86 @@ import "./style.css";
 
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const btn = document.getElementById("search-btn");
-const result = document.getElementById("result");
+const results = document.getElementById("results");
 const noResult = document.getElementById("no-result");
+const resultFound = document.getElementById("result-found");
+btn.addEventListener("click", () => {
+  const inpWord = document.getElementById("inp-word");
+  const wordAdd = document.getElementById("word-add");
+  console.log("Input word:", inpWord.value);
+  results.style.display = "block";
+  const word = document.getElementById("word-name");
+  if (inpWord.value != "") {
+    resultFound.style.display = "block";
+    wordAdd.style.display = "block";
+    word.innerHTML = inpWord.value;
+  } else {
+    wordAdd.style.display = "none";
+    word.innerHTML = "Sorry...couldn't find the word!!!";
+    resultFound.style.display = "none";
+  }
+});
 // const word = document.getElementById("word");
 // console.log(word);
 
 function playSound(sound) {
   sound.play();
 }
+//async added so that the function waits for the fetch rather than jumping to catch and displaying the sry message
+// btn.addEventListener("click", async () => {
+//   let inpWord = document.getElementById("inp-word").value;
+//   // console.log(inpWord);
+//   let word = document.getElementById("word");
+//   try {
+//     const x = await fetch(`${url}${inpWord}`);
+//     noResult.style.display = "none";
+//     result.style.display = "block";
+//     console.log("x.ok:", x.ok);
+//     if (!x.ok) {
+//       throw new Error("Status code error :" + x.status);
+//     }
+//     const data = await x.json();
+//     console.log("Data:", data);
+//     let start = Date.now();
+//     let details = document.querySelector("#details");
+//     console.log(details);
+//     let _details = details.querySelectorAll("p");
+//     let wordMeaning = document.getElementById("word-meaning");
+//     console.log("word meaning:", wordMeaning);
+//     let wordExample = document.getElementById("word-example");
+//     let soundBtn = document.getElementById("sound-btn");
+//     const noSoundBtn = document.getElementById("no_sound-btn");
 
-btn.addEventListener("click", async () => {
-  let inpWord = document.getElementById("inp-word").value;
-  // console.log(inpWord);
-  let word = document.getElementById("word");
-  try {
-    const x = await fetch(`${url}${inpWord}`);
-    noResult.style.display = "none";
-    result.style.display = "block";
-    console.log("x.ok:", x.ok);
-    if (!x.ok) {
-      throw new Error("Status code error :" + x.status);
-    }
-    const data = await x.json();
-    console.log("Data:", data);
-    let start = Date.now();
-    let details = document.querySelector("#details");
-    console.log(details);
-    let _details = details.querySelectorAll("p");
-    let wordMeaning = document.getElementById("word-meaning");
-    console.log("word meaning:", wordMeaning);
-    let wordExample = document.getElementById("word-example");
-    let soundBtn = document.getElementById("sound-btn");
-    const noSoundBtn = document.getElementById("no_sound-btn");
-    word.innerHTML = data[0].word;
-    _details[0].innerHTML = data[0].meanings[0].partOfSpeech;
-    _details[1].innerHTML = data[0].phonetic;
-    wordMeaning.innerHTML = data[0].meanings[0].definitions[0].definition;
-    wordExample.innerHTML = data[0].meanings[0].definitions[0].example || "";
-    let audio = data[0].phonetics[0].audio;
-    console.log("Audio:", audio);
-    if (audio != "") {
-      soundBtn.style.display = "block";
-      noSoundBtn.style.display = "none";
-      let soundSrc = new Audio(data[0].phonetics[0].audio);
-      console.log("Sound Src:", soundSrc);
-      soundBtn.onclick = () => {
-        soundSrc.play();
-      };
-    } else {
-      console.log("No sound");
-      soundBtn.style.display = "none";
-      noSoundBtn.style.display = "block";
-      console.log("soundBtn:", soundBtn);
-      console.log("nosoundBtn:", noSoundBtn);
-      // soundBtn.style.display = "none";
-    }
+//     word.innerHTML = data[0].word;
+//     _details[0].innerHTML = data[0].meanings[0].partOfSpeech;
+//     _details[1].innerHTML = data[0].phonetic;
+//     wordMeaning.innerHTML = data[0].meanings[0].definitions[0].definition;
+//     wordExample.innerHTML = data[0].meanings[0].definitions[0].example || "";
+//     let audio = data[0].phonetics[0].audio;
+//     console.log("Audio:", audio);
+//     if (audio != "") {
+//       soundBtn.style.display = "block";
+//       noSoundBtn.style.display = "none";
+//       let soundSrc = new Audio(data[0].phonetics[0].audio);
+//       console.log("Sound Src:", soundSrc);
+//       soundBtn.onclick = () => {
+//         soundSrc.play();
+//       };
+//     } else {
+//       console.log("No sound");
+//       soundBtn.style.display = "none";
+//       noSoundBtn.style.display = "block";
+//       console.log("soundBtn:", soundBtn);
+//       console.log("nosoundBtn:", noSoundBtn);
+//       // soundBtn.style.display = "none";
+//     }
 
-    let timeTaken = Date.now() - start;
-    console.log("Total time taken : " + timeTaken + " milliseconds");
-  } catch (error) {
-    result.style.display = "none";
-    noResult.style.display = "block";
-    noResult.innerHTML = `<h3>Sry...couldn't find the word!!</h3>`;
-    console.log("Error caught:", error);
-  }
-});
+//     let timeTaken = Date.now() - start;
+//     console.log("Total time taken : " + timeTaken + " milliseconds");
+//   } catch (error) {
+//     result.style.display = "none";
+//     noResult.style.display = "block";
+//     noResult.innerHTML = `<h3>Sry...couldn't find "${inpWord}" !!</h3>`;
+//     console.log("Error caught:", error);
+//   }
+// });
